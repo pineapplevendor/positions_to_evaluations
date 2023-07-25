d470472cabb504d60e00ce715d5618cbb44ae957
Evan Chavis <chavieva@amazon.com>
2023-07-25 14:29:03 -0400

    Update evaluations script to identify checkmates and tactics
    Note: the tactics identification is dubious but seems to work in most cases.
    It identifies positions where the previous eval to the current eval swung
    more than 300 centipawns in either direction. However, that could mean that
    there WAS a tactic on the board, but now there's not. Either way, the idea
    was to identify positions with tactical complications present and it's doing
    a good job of that.

--- get_evals_2.py
+++ get_evals_2.py
@@ -3,14 +3,14 @@ import chess.engine
 import sqlite3
 import json
 
-engine = chess.engine.SimpleEngine.popen_uci("/usr/bin/stockfish")
+engine = chess.engine.SimpleEngine.popen_uci("/usr/local/bin/stockfish")
 engine.configure({'Threads':8, "Hash": 1024})
 
-con = sqlite3.connect("evaluations.db")
+con = sqlite3.connect("lichess.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS evaluations(fen PRIMARY KEY, moves)")
+cur.execute("CREATE TABLE IF NOT EXISTS evaluations(fen PRIMARY KEY, moves, previous_moves, is_tactic, is_mate)")
 
-pgn_filename = "games.pgn"
+pgn_filename = "lichess_games.pgn"
 pgn = open(pgn_filename)
 
 game = chess.pgn.read_game(pgn)
@@ -18,27 +18,45 @@ games_count = 0
 moves_processed = 0
 moves_skipped = 0
 failed_to_process = 0
+
 while game:
+	previous_moves = []
+	previous_eval = None
+	current_eval = None
 	for node in game.mainline():
+		previous_moves.append(node.move)
 		board = node.board()
 		fen = node.board().fen()
 			
 		fen_search = cur.execute("SELECT * FROM evaluations WHERE fen=?", (fen,))
 		is_fen_searched = fen_search.fetchone() is not None
 		if not is_fen_searched:
-			analysis = engine.analyse(node.board(), chess.engine.Limit(nodes=100000), multipv=3)
 			try:
+				analysis = engine.analyse(node.board(), chess.engine.Limit(nodes=100000), multipv=3)
+				score = analysis[0]['score']
+				current_eval = score.white().score()
 				moves_and_evals = [{"eval": str(a['score']), "move": str(a["pv"][0])} for a in analysis]
-				data = [(fen, json.dumps(moves_and_evals))]
-				cur.executemany("INSERT INTO evaluations VALUES(?, ?)", data)
+				uci_moves = str([m.uci() for m in previous_moves])
+
+				is_tactic = current_eval and previous_eval and abs(current_eval - previous_eval) > 300
+				is_mate = score.is_mate()
+
+				data = [(fen, json.dumps(moves_and_evals), uci_moves, is_tactic, is_mate)]
+				cur.executemany("INSERT INTO evaluations VALUES(?, ?, ?, ?, ?)", data)
 				moves_processed += 1
-			except Exception:
-				print(analysis)
+				previous_eval = current_eval
+			except Exception as e:
+				print(fen)
+				print('exception', e)
 				failed_to_process += 1
 		else:
+			previous_eval = None
+			current_eval = None
 			moves_skipped += 1
 		con.commit()
 	games_count += 1
 	game = chess.pgn.read_game(pgn)
-	print("games processed: ", games_count, "moves processed: ", moves_processed, "moves skipped: ", moves_skipped, "failed: ", failed_to_process)
+	print("games processed: ", games_count, "moves processed: ", 
+		moves_processed, "moves skipped: ", moves_skipped, "failed: ", failed_to_process)
+
 engine.quit()
